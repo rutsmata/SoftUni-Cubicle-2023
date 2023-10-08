520555496b1398634b589607beb81f04bdc05bbb
Rumen Nazarov <rustmata@gmail.com>
2023-10-08 13:41:16 +0300

    get cubes from db

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,27 +1,7 @@
 const Cube = require("../models/Cube");
 
-const uniqid = require("uniqid");
-const cubes = [
-  {
-    id: "h3h38sad",
-    name: "Mirror Cube",
-    description: "Descr Mirror Cube",
-    imageUrl:
-      "https://media.istockphoto.com/id/473622694/photo/rubiks-cube.jpg?s=612x612&w=0&k=20&c=jG8vgbrd8jsLPYwzNTgoALf86E20dbTBC30S77QjyhY=",
-    difficultyLevel: 5,
-  },
-  {
-    id: "5d22d5ss4d5f52d2d2d6",
-    name: "Rubik Cube",
-    description: "Descr Rubik Cube",
-    imageUrl:
-      "https://w7.pngwing.com/pngs/590/189/png-transparent-rubik-s-cube-puzzle-three-dimensional-space-cube-thumbnail.png",
-    difficultyLevel: 5,
-  },
-];
-
-exports.getAll = (search, from, to) => {
-  let result = cubes.slice();
+exports.getAll = async (search, from, to) => {
+  let result = await Cube.find().lean();
 
   if (search) {
     result = result.filter((cube) =>
@@ -40,7 +20,7 @@ exports.getAll = (search, from, to) => {
   return result;
 };
 
-exports.getOne = (cubeId) => cubes.find((x) => x.id === cubeId);
+exports.getOne = (cubeId) => Cube.findById(cubeId);
 
 exports.create = async (cubeData) => {
   const cube = new Cube(cubeData);
